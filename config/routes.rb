86b77c2e32acc15746a487fb740cf1852301c7f4
Monica Valluri <monica@nestaway.com>
2018-08-10 17:03:14 +0530

    added signup page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
+  get 'signup' => 'users#new'
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
